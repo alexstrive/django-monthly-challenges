dd9d975544ae09c9b2d756d41773b1aaee1ec23b
Alex Strive <alexei.novopashin@gmail.com>
2023-01-03 11:35:53 -0800

    remove redundant import

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 challenges = {
