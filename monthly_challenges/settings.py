f8c27da09f93f670226f92d9842dc01b2fd2026b
Alex Strive <alexei.novopashin@gmail.com>
2023-01-04 03:36:10 -0800

    fix settings.py
    adapt usage of template blocks

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -53,8 +53,9 @@ ROOT_URLCONF = 'monthly_challenges.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
